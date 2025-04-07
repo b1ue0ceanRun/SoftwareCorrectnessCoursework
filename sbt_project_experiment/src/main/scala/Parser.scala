067096fc517a1b497a2da84286a7f2d6d1cac8b1
au548789 <au548789@uni.au.dk>
2025-04-07 14:20:55 +0200

    small fixes so Hanka can run it

--- sbt_project_experiment/src/main/scala/Parser.scala
+++ sbt_project_experiment/src/main/scala/Parser.scala
@@ -1,9 +1,10 @@
 
 class Parser(state: State) {
     def receiveCode(code: String): Unit = {      
-      code match
+      code match {
         case "circle" => state.updateState(true)
         case _ => println(s"ERROR / Not implemented")
+    }
     }
 }
 
